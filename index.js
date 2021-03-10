196f5eef4dddeda6523c43b906fbf62172a15ac6
Alex Waller <alex@artofalexwaller.com>
2021-03-10 13:19:38 -0500

    form eventlisteners

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/gas_stations"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getStations()
+
+    const createGasStationForm = document.querySelector("#create-gas_station-form")
+
+    createGasStationForm.addEventListener("submit", (e) => console.log(e))
+})
+
+function getStations() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(stations => {
+        stations.data.forEach(gas_station => {
+
+
+            const stationMarkup = 
+            <div data-id={gas_station.id}>
+                <img src={gas_station.attributes.image}
+                height="200" width="250">
+                    
+                    <p>{gas_station.attributes.state.name}</p>
+                    <button data-id={gas_station.id}>edit</button>
+            </div>
+            <br></br> ;
+
+            document.querySelector('#gas_station-
+            container').innerHTML += syllabusMarkup
+        })
+    })
+}
